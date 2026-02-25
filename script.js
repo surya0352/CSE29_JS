4d712909e49b4013f7748e724a8ea5b2cf9d460f
Suryakantray <surya.25b01010352@abes.ac.in>
2026-02-25 14:46:04 +0530

    arithmatic operation

--- script.js
+++ script.js
@@ -1,37 +1,74 @@
 console.log("Hello World")
 document.write("Hello World!")
-var linebreak="<br>";
+var linebreak = "<br>";
 document.write(linebreak);
-var num =10;
+var num = 10;
 console.log(num);
-console.log(typeof(num));
-num=false;
+console.log(typeof (num));
+num = false;
 console.log(num);
-console.log(typeof(num));
-var num ="surya";
+console.log(typeof (num));
+var num = "surya";
 console.log(num);
-console.log(typeof(num));
-var num =null;
+console.log(typeof (num));
+var num = null;
 console.log(num);
-console.log(typeof(num));
-var num =undefined;
+console.log(typeof (num));
+var num = undefined;
 console.log(num);
-console.log(typeof(num));
+console.log(typeof (num));
 
-const sym1=Symbol(4);
+var sayHello = function () {
+    alert('hello world!');
+}
+var numberArray = [1, 2, 3];
+var animals = new Array("cat", "dog", "mouse", "lion");
+var person = {
+    name: 'Barack Hussein Obama II',
+    age: '51',
+    title: '44th President of the United States'
+}
+
+const sym1 = Symbol(4);
 console.log(sym1);
-const sym2=Symbol(4);
+const sym2 = Symbol(4);
 console.log(sym2);
-if (sym1==sym2){
+if (sym1 == sym2) {
     console.log("true")
 }
-else{
+else {
     console.log("false")
 }
 
 
-var a=100; var b=200;var c=150;var linebreak="<br>";
+var a = 100; var b = 200; var c = 150; var linebreak = "<br>";
 document.write("a+b+c=");
-result=a+b+c;
+result = a + b + c;
 document.write(result);
 document.write(linebreak)
+
+document.write("a + b = ");
+result = a + b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a - b = ");
+result = a - b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a * b = ");
+result = a * b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a / b = ");
+result = a / b;
+document.write(result);
+document.write(linebreak);
+
+document.write("a % b = ");
+result = a % b;
+document.write(result);
+document.write(linebreak);
+
