712a66685c1c1b62e4139a844ac339f0f263953c
Suryakantray <surya.25b01010352@abes.ac.in>
2026-02-25 14:08:45 +0530

    learn diffrenet data types

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("Hello World")
+document.write("Hello World!")
+
+var num =10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+var num ="surya";
+console.log(num);
+console.log(typeof(num));
+var num =null;
+console.log(num);
+console.log(typeof(num));
+var num =undefined;
+console.log(num);
+console.log(typeof(num));
+
+const sym1=Symbol(4);
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if (sym1==sym2){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
