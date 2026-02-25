e32e1f4207455baea7bb870d7309aab4140086e5
Suryakantray <surya.25b01010352@abes.ac.in>
2026-02-25 14:47:26 +0530

    assignment  operation

--- script.js
+++ script.js
@@ -102,3 +102,29 @@ result = (a <= b);
 document.write(result);
 document.write(linebreak);
 
+document.write("Value of a => (a = b) => ");
+result = (a = b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Value of a => (a += b) => ");
+result = (a += b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Value of a => (a -= b) => ");
+result = (a -= b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Value of a => (a *= b) => ");
+result = (a *= b);
+document.write(result);
+document.write(linebreak);
+
+document.write("Value of a => (a /= b) => ");
+result = (a /= b);
+document.write(result);
+document.write(linebreak);
+
+
