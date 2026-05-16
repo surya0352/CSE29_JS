a6d36c117ba5f876b60556135c5dca5116e693fd
Suryakantray <surya.25b01010352@abes.ac.in>
2026-05-16 23:34:45 +0530

    DOM

---
+++ DOM/DOM.js
@@ -0,0 +1,26 @@
+function onclick() {
+    alert("Button clicked!");
+}
+
+function onmouseout() {
+    alert(" on Mouse out ");
+}
+
+function onmousedown() {
+    alert("on Mouse down");
+}
+
+function onmouseup() {
+    alert("on Mouse up");
+}
+
+function onmouseremove() {
+    alert("on Mouse remove ");
+}
+
+function ondblclick() {
+    alert("on Button doubleclicked!");
+}
+function myfunction(){
+    alert("window load successful");
+}
